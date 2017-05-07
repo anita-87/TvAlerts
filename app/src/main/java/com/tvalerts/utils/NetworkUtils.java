c87c286f686e2c43b3a0759cb7c00dc4a7842505
anita-87 <ana.rodriguez.fernandez.87@gmail.com>
2017-05-07 22:18:25 +0200

    - Updated the NetworkUtils getAllShows query, since it is a paginated query. The old method was not retrieving all the shows.

--- app/src/main/java/com/tvalerts/utils/NetworkUtils.java
+++ app/src/main/java/com/tvalerts/utils/NetworkUtils.java
@@ -4,9 +4,12 @@ import android.util.Log;
 
 import com.tvalerts.domain.Show;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,15 +21,28 @@ import java.util.List;
 public final class NetworkUtils {
 
     private static final String TAG = NetworkUtils.class.getSimpleName();
-    private static final String SHOWS_URL = "http://api.tvmaze.com/shows";
+    private static final String SHOWS_URL = "http://api.tvmaze.com/shows?page=";
 
     private static RestTemplate restTemplate;
 
     public static List<Show> getAllShows() {
         initRestTemplate();
-        Show[] shows = restTemplate.getForObject(SHOWS_URL, Show[].class);
-        Log.d(TAG, "Shows received: " + shows.length);
-        return Arrays.asList(shows);
+        List<Show> result = new ArrayList<>();
+        boolean moreResults = true;
+        int page = 1;
+        Log.d(TAG, "Starting the search");
+        while (moreResults) {
+            try {
+                Show[] shows = restTemplate.getForObject(SHOWS_URL + page, Show[].class);
+                result.addAll(Arrays.asList(shows));
+                page++;
+            } catch (HttpClientErrorException e) {
+                if (e.getStatusCode() == HttpStatus.NOT_FOUND)
+                    moreResults = false;
+            }
+        }
+        Log.d(TAG, "Shows received: " + result.size());
+        return result;
     }
 
     private static void initRestTemplate() {
